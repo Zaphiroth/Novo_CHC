6918d33d7dbe1f7764c1ec9ee7e7885e31c36552
Zaphiroth <zliu@pharbers.com>
2020-12-16 19:08:32 +0800

    readin

---
+++ 04_Codes/01_Readin_and_Format_Data.R
@@ -0,0 +1,194 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ProjectName:  Novo Insulin CHC
+# Purpose:      Readin
+# programmer:   Zhe Liu
+# Date:         2020-12-16
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+##---- Universe info ----
+## PCHC info
+pchc.mapping <- read.xlsx("02_Inputs/Universe_PCHCCode_20201209.xlsx", sheet = "PCHC")
+
+pchc.mapping1 <- pchc.mapping %>% 
+  filter(!is.na(`单位名称`), !is.na(PCHC_Code)) %>% 
+  group_by(province = `省`, city = `地级市`, district = `区[县/县级市】`, hospital = `单位名称`) %>% 
+  summarise(pchc = first(PCHC_Code)) %>% 
+  ungroup()
+
+pchc.mapping2 <- pchc.mapping %>% 
+  filter(!is.na(ZS_Servier.name), !is.na(PCHC_Code)) %>% 
+  group_by(province = `省`, city = `地级市`, district = `区[县/县级市】`, hospital = `ZS_Servier.name`) %>% 
+  summarise(pchc = first(PCHC_Code)) %>% 
+  ungroup()
+
+pchc.mapping3 <- bind_rows(pchc.mapping1, pchc.mapping2) %>% 
+  distinct(province, city, district, hospital, pchc)
+
+pchc.mapping4 <- pchc.mapping3 %>% 
+  group_by(pchc) %>% 
+  summarise(province = first(na.omit(province)),
+            city = first(na.omit(city)),
+            district = first(na.omit(district))) %>% 
+  ungroup()
+
+## CHPA
+std.info <- read.xlsx('02_Inputs/Product standardization master data-A-S-1211.xlsx') %>% 
+  distinct(corp = CORP_NAME_EN, type = MNF_TYPE, atc3 = ATC3_CODE, atc4 = ATC4_CODE, 
+           atc3_cn = ATC3, molecule = MOLE_NAME_EN, molecule_cn = MOLE_NAME_CH, 
+           product = PROD_DESC, product_cn = PROD_NAME_CH, pack = PCK_DESC, 
+           route = NFC1_NAME_CH, packid = PACK_ID)
+
+## market definition
+market.def <- read_xlsx("02_Inputs/市场分子式明细_chk_20201127.xlsx") %>% 
+  distinct(atc3 = ATCIII.Code, molecule = Molecule.Composition.Name, market = TC) %>% 
+  left_join(std.info, by = c('atc3', 'molecule')) %>% 
+  filter(!is.na(packid))
+
+
+##---- Raw data ----
+## Servier
+raw.servier1 <- read_csv('02_Inputs/data/Servier_ahbjjssdzj_17181920Q1Q2Q3_fj1718_nozj20Q3_packid_moleinfo.csv', 
+                        locale = locale(encoding = "GB18030")) %>% 
+  filter(stri_sub(ATC4_Code, 1, 4) %in% c('A10S'))
+
+raw.servier <- raw.servier %>% 
+  distinct(year = as.character(Year), 
+           quarter = Quarter, 
+           date = as.character(Month), 
+           province = gsub('省|市', '', Province), 
+           city = if_else(City == "市辖区", "北京", gsub("市", "", City)), 
+           district = County, 
+           hospital = Hospital_Name, 
+           packid = stri_pad_left(packcode, 7, 0), 
+           units = if_else(is.na(Volume), Value / Price, Volume), 
+           sales = Value) %>% 
+  left_join(pchc.mapping3, by = c('province', 'city', 'district', 'hospital')) %>% 
+  filter(!is.na(pchc)) %>% 
+  mutate(packid = if_else(stri_sub(packid, 1, 5) == '47775', 
+                          stri_paste('58906', stri_sub(packid, 6, 7)), 
+                          packid), 
+         packid = if_else(stri_sub(packid, 1, 5) == '06470', 
+                          stri_paste('64895', stri_sub(packid, 6, 7)), 
+                          packid)) %>% 
+  filter(units > 0, sales > 0) %>% 
+  select(year, date, quarter, province, city, district, pchc, packid, units, sales)
+
+## 胰岛素
+raw.yds1 <- read_csv('02_Inputs/data/yidaosu_ahbjjssdzj171819_fj1718_packid_moleinfo.csv', 
+                     locale = locale(encoding = "GB18030")) %>% 
+  filter(stri_sub(ATC4_Code, 1, 4) %in% c('A10C', 'A10D'))
+
+raw.yds2 <- read_csv('02_Inputs/data/yidaosu_ahbjjssdzj_2020Q1Q2_ahbjjs20Q3_packid_moleinfo.csv', 
+                     locale = locale(encoding = "GB18030")) %>% 
+  filter(stri_sub(ATC4_Code, 1, 4) %in% c('A10C', 'A10D'))
+
+raw.yds3 <- read_csv('02_Inputs/data/yidaosu_sd_20Q3_packid_moleinfo.csv', 
+                     locale = locale(encoding = "GB18030")) %>% 
+  filter(stri_sub(ATC4_Code, 1, 4) %in% c('A10C', 'A10D'))
+
+raw.yds <- bind_rows(raw.yds1, raw.yds2, raw.yds3) %>% 
+  distinct(year = as.character(Year), 
+           quarter = Quarter, 
+           date = as.character(Month), 
+           province = gsub('省|市', '', Province), 
+           city = if_else(City == "市辖区", "北京", gsub("市", "", City)), 
+           district = County, 
+           hospital = Hospital_Name, 
+           packid = stri_pad_left(packcode, 7, 0), 
+           units = if_else(is.na(Volume), Value / Price, Volume), 
+           sales = Value) %>% 
+  left_join(pchc.mapping3, by = c('province', 'city', 'district', 'hospital')) %>% 
+  filter(!is.na(pchc)) %>% 
+  mutate(packid = if_else(stri_sub(packid, 1, 5) == '47775', 
+                          stri_paste('58906', stri_sub(packid, 6, 7)), 
+                          packid), 
+         packid = if_else(stri_sub(packid, 1, 5) == '06470', 
+                          stri_paste('64895', stri_sub(packid, 6, 7)), 
+                          packid)) %>% 
+  filter(units > 0, sales > 0) %>% 
+  select(year, date, quarter, province, city, district, pchc, packid, units, sales)
+
+
+
+
+
+## Guangzhou
+raw.gz1 <- read_feather('02_Inputs/data/广州/Servier_guangzhou_17181920Q1Q2_packid_moleinfo.feather')
+
+raw.gz <- raw.gz1 %>% 
+  distinct(year = as.character(Year), 
+           quarter = Quarter, 
+           date = as.character(Month), 
+           province = '广东', 
+           city = '广州', 
+           hospital = Hospital_Name, 
+           packid = stri_pad_left(packcode, 7, 0), 
+           price = Price, 
+           units = if_else(is.na(Volume), Value / Price, Volume), 
+           sales = Value) %>% 
+  left_join(pchc.mapping3, by = c('province', 'city', 'hospital')) %>% 
+  filter(!is.na(pchc), stri_sub(packid, 1, 5) %in% stri_sub(market.def$packid, 1, 5)) %>% 
+  mutate(packid = if_else(stri_sub(packid, 1, 5) == '47775', 
+                          stri_paste('58906', stri_sub(packid, 6, 7)), 
+                          packid), 
+         packid = if_else(stri_sub(packid, 1, 5) == '06470', 
+                          stri_paste('64895', stri_sub(packid, 6, 7)), 
+                          packid)) %>% 
+  filter(units > 0, sales > 0) %>% 
+  select(year, date, quarter, province, city, district, pchc, packid, units, sales)
+
+## Shanghai
+raw.sh1 <- read.xlsx('02_Inputs/data/上海/上海_2017.xlsx')
+raw.sh2 <- read.xlsx('02_Inputs/data/上海/上海_2018.xlsx')
+
+raw.sh <- bind_rows(raw.sh1, raw.sh2) %>% 
+  mutate(quarter_m = stri_sub(Date, 5, 6)) %>% 
+  distinct(year = stri_sub(Date, 1, 4), 
+           quarter = ifelse(quarter_m %in% c('01', '02', '03'), 
+                            stri_paste(year, 'Q1'), 
+                            ifelse(quarter_m %in% c('04', '05', '06'), 
+                                   stri_paste(year, 'Q2'), 
+                                   ifelse(quarter_m %in% c('07', '08', '09'), 
+                                          stri_paste(year, 'Q3'), 
+                                          ifelse(quarter_m %in% c('10', '11', '12'), 
+                                                 stri_paste(year, 'Q4'), 
+                                                 year)))), 
+           date = as.character(Date), 
+           province = '上海', 
+           city = '上海', 
+           pchc = PCHC, 
+           packid = stri_pad_left(pfc, 7, 0), 
+           price = value / unit, 
+           units = unit, 
+           sales = value) %>% 
+  mutate(pchc = case_when(pchc == 'PCHC06729' ~ 'PCHC06728', 
+                          pchc == 'PCHC06622' ~ 'PCHC06620', 
+                          pchc == 'PCHC06645' ~ 'PCHC06644', 
+                          pchc == 'PCHC06722' ~ 'PCHC06721', 
+                          pchc == 'PCHC06840' ~ 'PCHC06839', 
+                          TRUE ~ pchc)) %>% 
+  left_join(pchc.mapping4, by = c('province', 'city', 'pchc')) %>% 
+  filter(pchc != '#N/A', stri_sub(packid, 1, 5) %in% stri_sub(market.def$packid, 1, 5)) %>% 
+  mutate(packid = if_else(stri_sub(packid, 1, 5) == '47775', 
+                          stri_paste('58906', stri_sub(packid, 6, 7)), 
+                          packid), 
+         packid = if_else(stri_sub(packid, 1, 5) == '06470', 
+                          stri_paste('64895', stri_sub(packid, 6, 7)), 
+                          packid)) %>% 
+  filter(units > 0, sales > 0) %>% 
+  select(year, date, quarter, province, city, district, pchc, packid, units, sales)
+
+## total
+raw.total <- bind_rows(raw.data, raw.gz, raw.sh) %>% 
+  group_by(pchc) %>% 
+  mutate(province = first(na.omit(province)), 
+         city = first(na.omit(city)), 
+         district = first(na.omit(district))) %>% 
+  ungroup() %>% 
+  group_by(year, date, quarter, province, city, district, pchc, packid) %>% 
+  summarise(units = sum(units, na.rm = TRUE), 
+            sales = sum(sales, na.rm = TRUE)) %>% 
+  ungroup()
+
+write_feather(raw.total, '03_Outputs/Servier_CHC2_Raw.feather')
